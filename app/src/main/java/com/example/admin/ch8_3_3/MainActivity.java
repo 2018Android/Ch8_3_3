1fd7b773123ae785402a2f1348d4554628b878c1
chenyirou <annie851216@gmail.com>
2018-05-29 12:04:52 +0800

    test OK

--- app/src/main/java/com/example/admin/ch8_3_3/MainActivity.java
+++ app/src/main/java/com/example/admin/ch8_3_3/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.admin.ch8_3_3;
 
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HandlerFunction();
+        Button btn_progress = (Button)findViewById(R.id.btn_progress);
+        btn_progress.setOnClickListener(btn_progress_Click);
     }
+
+    //---------------------------Progress Fragment-------------------------
+    public void btn_progress_do_P_Click(){
+        Toast.makeText(this, "隱藏下載對話方塊", Toast.LENGTH_SHORT).show();
+    }
+
+    public void btn_progress_do_N_Click(){
+        Toast.makeText(this, "取消下載", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private ProgressDialogFragment dlg;
+    private int p=0;
+    private Handler pHandler;
+
+    private void HandlerFunction(){
+        pHandler = new Handler(){
+            public void handleMessage(Message msg){
+                super.handleMessage(msg);
+                if(p>100){
+                    dlg.dismiss();
+                    TextView txv_progressFinish = (TextView)findViewById(R.id.txv_progressFinish);
+                    txv_progressFinish.setText("下載已完成");
+                }else{
+                    p++;
+                    dlg.updateProgress();
+                    pHandler.sendEmptyMessageDelayed(0, 50);
+                }
+            }
+        };
+    }
+
+    View.OnClickListener btn_progress_Click = new View.OnClickListener(){
+        public void onClick(View v){
+            dlg = ProgressDialogFragment.newInstance("下載檔案");
+            FragmentManager fm = getSupportFragmentManager();
+            dlg.show(fm, "progressdialog");
+            p=0;
+            pHandler.sendEmptyMessage(0);
+        }
+    };
 }
--- app/src/main/java/com/example/admin/ch8_3_3/ProgressDialogFragment.java
+++ app/src/main/java/com/example/admin/ch8_3_3/ProgressDialogFragment.java
@@ -1,7 +1,11 @@
 package com.example.admin.ch8_3_3;
 
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,8 +15,43 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProgressDialogFragment extends Fragment {
+public class ProgressDialogFragment extends DialogFragment {
+    private ProgressDialog pDialog;
 
+    static ProgressDialogFragment newInstance(String title){
+        ProgressDialogFragment dlg = new ProgressDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title",title);
+        dlg.setArguments(args);                                         //設定參數
+        return dlg;
+    }
+
+    DialogInterface.OnClickListener p_click = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialog, int which){
+            ((MainActivity)getActivity()).btn_progress_do_P_Click();
+        }
+    };
+
+    DialogInterface.OnClickListener n_click = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialog, int which){
+            ((MainActivity)getActivity()).btn_progress_do_N_Click();
+        }
+    };
+
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        String title = getArguments().getString("title");
+        pDialog = new ProgressDialog(getActivity());
+        pDialog.setTitle(title);
+        pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        pDialog.setProgress(0);
+        pDialog.setButton(DialogInterface.BUTTON_POSITIVE, "隱藏",p_click);
+        pDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"取消",n_click);
+        return pDialog;
+    }
+    // 更新 ProgressDialog 顯示的進度，每呼叫1次 就加1
+    public void updateProgress(){
+        pDialog.incrementProgressBy(1);
+    }
 
     public ProgressDialogFragment() {
         // Required empty public constructor
